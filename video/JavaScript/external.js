a81c366c7eadc30ee33d8f64fbec543ef124320a
abrahamthankGod <152361931+abrahamthankGod@users.noreply.github.com>
2023-12-05 01:38:31 -0800

    batch31 web
    batch31 web project

---
+++ video/JavaScript/external.js
@@ -0,0 +1,34 @@
+
+function Message(){
+alert("Your location is being tracked!")	
+}
+
+function Confirm(){
+confirm("are you over 18 ?")
+}
+
+
+function Enquiry(){
+prompt("Enter your email")	
+}
+function change(){
+txt="hello world"
+document.getElementById('display').innerHTML=txt
+console.log(txt)
+}
+
+
+function change01(){
+txt="We are learning web design"
+document.getElementById('display').innerHTML=txt
+console.log(txt)
+}change()
+
+
+
+
+
+result=10<23
+Result=10<23
+console.log()
+//this is a code
